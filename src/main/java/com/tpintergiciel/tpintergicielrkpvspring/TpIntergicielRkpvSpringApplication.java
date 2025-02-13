e43024d1cb0bdf83a944e7455ade97f08f224263
Yelauu <127487673+ThomasVly@users.noreply.github.com>
2025-02-13 14:59:28 +0100

    feat : initial commit

---
+++ src/main/java/com/tpintergiciel/tpintergicielrkpvspring/TpIntergicielRkpvSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tpintergiciel.tpintergicielrkpvspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntergicielRkpvSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntergicielRkpvSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpintergiciel/tpintergicielrkpvspring/TpIntergicielRkpvSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpintergiciel.tpintergicielrkpvspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntergicielRkpvSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
