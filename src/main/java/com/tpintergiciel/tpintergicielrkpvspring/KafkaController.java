99e91b09b329f62b615e306dff41aea3355b2778
Yelauu <127487673+ThomasVly@users.noreply.github.com>
2025-02-14 10:02:06 +0100

    feat : on client send to kafka, and the other one listen and consume

---
+++ src/main/java/com/tpintergiciel/tpintergicielrkpvspring/KafkaController.java
@@ -0,0 +1,24 @@
+package com.tpintergiciel.tpintergicielrkpvspring;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class KafkaController {
+
+    private final KafkaProducer producer;
+
+    public KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping("/publish")
+    public void writeMessageToTopic(@RequestParam("message") String message){
+        this.producer.writeMessage(message);
+
+    }
+
+}
+
+
